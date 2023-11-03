0c383a017cdc9458576c1e0906a9525be78f609a
sixel584 <149739476+sixel584@users.noreply.github.com>
2023-11-02 21:47:59 -0500

    Add files via upload

---
+++ Primeraweb/js/script.js
@@ -0,0 +1,79 @@
+/* function retornarFecha() {
+    let fecha;
+    fecha = new Date();
+    var cadena = fecha.getDate() + '/' + (fecha.getMonth() + 1) + '/' + fecha.getFullYear();
+    return cadena;
+}
+
+function retornarHora() {
+    let fecha;
+    fecha = new Date();
+    var cadena = fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
+    return cadena;
+}*/
+
+// SELECCIONAR ELEMENTOS
+document.addEventListener("DOMContentLoaded", function() {
+  let contenidoTitulo = "Nombre";
+
+  // Error 1: Corregir la selección del elemento por ID.
+  let titulo = document.getElementById("titulo1");
+  
+  // Error 2: Agregar "titulo." para establecer el innerHTML.
+  titulo.innerHTML = contenidoTitulo;
+
+  // CONDICIONALES
+  let textoTitulo = titulo.innerText;
+  console.log(textoTitulo);
+
+  if (textoTitulo == "Nombre") {
+    // Error 3: Corregir la actualización de innerHTML.
+    titulo.innerHTML = "CORTE Y CONFECCION DE ROPA DEPORTIVA";
+  } else {
+    console.log("No se cumple");
+  }
+});
+/* document.addEventListener("DOMContentLoaded", function() {
+    // Tu código JavaScript aquí
+    let nombre = "SSOLARTE";
+    let ciudad = "POPAYAN";
+    let gusto = "CAFE";
+
+    let elementos = document.getElementsByClassName("informacion");
+
+    function cambiarTexto() {
+        let contenido = `Mi nombre es ${nombre}, vivo en la ciudad de Popayán, y me gusta el ${gusto}. Me gradué del Instituto de Tecnología de Florida en 2020 con una licenciatura en desarrollo de software. Mientras estaba en la escuela, obtuve diferentes reconocimientos por mi desempeño académico ejemplar y mis habilidades de liderazgo.`;
+
+        return contenido;
+    }
+
+    // Itera a través de los elementos y establece el texto en cada uno
+    for (let i = 0; i < elementos.length; i++) {
+        elementos[i].innerText = cambiarTexto();
+    }
+});
+
+ */
+document.addEventListener("DOMContentLoaded", function() {
+let nombre="SSOLARTE";
+let ciudad="Popayan";
+let gusto="Cafe";
+
+    let parrafo = document.querySelector(".informacion");
+function cambiarTexto(nombre, ciudad, gusto){ 
+let contenido = `Mi nombre es ${nombre}, vivo en la ciudad de ${ciudad}, y me gusta el ${gusto}. Me gradué del Centro Educativo Empresarial Comfacauca en 2015. Mientras estaba en la escuela.`;
+
+return contenido;
+}
+parrafo.innerText = cambiarTexto(nombre, ciudad, gusto);
+
+ });
+// SELECCIONAR NAVBAR MEDIANTE JAVASCRIPT
+document.addEventListener("DOMContentLoaded", function(){
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+}
+});
+ 
---
+++ Primeraweb/js/script2.js
@@ -0,0 +1,23 @@
+alert("Hola este es mi Javascript");
+
+// let nombre = "Sebas";
+//  nombre ="Sebas";
+
+//  var nombre1 = "Otro Ejemplo";
+
+// const nombre2 = "Tercer ejemplo";
+
+
+//  console.log(nombre);
+// console.log(nombre1);
+
+// SELECCIONAR ELEMENTOS EN JAVASCRIPT
+
+let contenidoTitulo =   "Nombre";
+let titulo = document.getElementById("Titulo3").innerHTML;
+tituloinnerHTML = contenidoTitulo;
+
+// CONDICIONALES//
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
